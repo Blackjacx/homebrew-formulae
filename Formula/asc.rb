e158aafaf990749a5f49292d6b2103dce18bfdf7
Stefan Herold <stefan.herold@gmail.com>
2023-08-24 18:58:38 +0200

    ASC Bump to 0.4.0

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -1,7 +1,7 @@
 class Asc < Formula
   desc "App Store Connect API access via Swift command-line tool"
   homepage "https://github.com/Blackjacx/Assist"
-  url "https://github.com/Blackjacx/Assist.git", tag: "0.3.0", revision: "2174f2d457fe8a93cbaca0fcdab05297c1326841"
+  url "https://github.com/Blackjacx/Assist.git", tag: "0.4.0", revision: "f89e16c329d1277ad2f3837cb4ee8087abc450c6"
   license "MIT"
   head "https://github.com/Blackjacx/Assist.git", branch: "develop"
 
