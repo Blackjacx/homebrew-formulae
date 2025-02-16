5cbec6571e4e7957407a848023f2533d521c9af3
Stefan Herold <Blackjacx@users.noreply.github.com>
2025-02-16 16:58:03 +0100

    Update asc.rb

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -1,12 +1,12 @@
 class Asc < Formula
   desc "App Store Connect API access via Swift command-line tool"
   homepage "https://github.com/Blackjacx/Assist"
-  url "https://github.com/Blackjacx/Assist.git", tag: "0.5.1", revision: "65a7592e2953373539fab1301d2ce7a8d2a0a238"
+  url "https://github.com/Blackjacx/Assist.git", tag: "0.5.2", revision: "0670c46cf5f110b2ba88097d3e88008f13f97a93"
   license "MIT"
   head "https://github.com/Blackjacx/Assist.git", branch: "develop"
 
-  depends_on xcode: ["15.0", :build]
-  depends_on macos: :ventura
+  depends_on xcode: ["16.0", :build]
+  depends_on macos: :sequoia
 
   def install
     system "make", "install", "prefix=#{prefix}"
