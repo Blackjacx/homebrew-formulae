d001e821b72db1f6ef847302297a3673ec235f61
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-02-17 04:25:20 +0100

    asc 0.6.0

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -1,8 +1,8 @@
 class Asc < Formula
   desc "App Store Connect API access via Swift command-line tool"
   homepage "https://github.com/Blackjacx/Assist"
-  url "https://github.com/Blackjacx/Assist/archive/refs/tags/0.5.3.tar.gz"
-  sha256 "fbb44c10f4adf6f5ea90bfda015cad26abd9a61741ddae400c14ce40fe49dcbc"
+  url "https://github.com/Blackjacx/Assist/archive/refs/tags/0.6.0.tar.gz"
+  sha256 "b392bbc9273d5d0653d02be556567adfc274741a8e0562edc2f738b44f094f41"
   license "MIT"
   head "https://github.com/Blackjacx/Assist.git", branch: "develop"
 
